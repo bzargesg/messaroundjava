5d28e8ec0efa06e1aac9722d4a925dc550f9b5b3
bzargesg <bzargesg@gmail.com>
2019-10-14 06:16:11 -0600

    hashmap messaround

--- messaround/src/app/App.java
+++ messaround/src/app/App.java
@@ -1,19 +1,38 @@
 package app;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ArrayList<String> names = new ArrayList<>();
 
-        names.add("George");
-        names.add("Kevin");
-        // System.out.println(names);
+        /**
+         * ARRAYS
+         */
+        // ArrayList<String> names = new ArrayList<>();
+        // names.add("George");
+        // names.add("Kevin");
+        // // System.out.println(names);
 
-        // for (String name : names) {
-        // System.out.println(name);
-        // }
+        // // for (String name : names) {
+        // // System.out.println(name);
+        // // }
+        // names.forEach(System.out::println);
 
-        names.forEach(System.out::println);
+        /*
+         * HASHMAPS
+         */
+
+        HashMap<String, Integer> numbers = new HashMap();
+        numbers.put("zero", 0);
+        numbers.put("one", 1);
+        numbers.put("two", 2);
+
+        // System.out.println(numbers);
+        // numbers.forEach((key, value) -> {
+        // System.out.println("Key: " + key);
+        // System.out.println("Value: " + value);
+        // });
+
     }
 }
