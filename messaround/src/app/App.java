01746f3591334818b5e5674b6390c3c58c38c2db
bzargesg <bzargesg@gmail.com>
2019-10-15 06:19:27 -0600

    cleanup

--- messaround/src/app/App.java
+++ messaround/src/app/App.java
@@ -6,33 +6,6 @@ import java.util.HashMap;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        /**
-         * ARRAYS
-         */
-        // ArrayList<String> names = new ArrayList<>();
-        // names.add("George");
-        // names.add("Kevin");
-        // // System.out.println(names);
-
-        // // for (String name : names) {
-        // // System.out.println(name);
-        // // }
-        // names.forEach(System.out::println);
-
-        /*
-         * HASHMAPS
-         */
-
-        HashMap<String, Integer> numbers = new HashMap();
-        numbers.put("zero", 0);
-        numbers.put("one", 1);
-        numbers.put("two", 2);
-
-        // System.out.println(numbers);
-        // numbers.forEach((key, value) -> {
-        // System.out.println("Key: " + key);
-        // System.out.println("Value: " + value);
-        // });
-
     }
+
 }
